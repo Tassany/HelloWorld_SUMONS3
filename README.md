3192015f30086735f10fd8760331cfa98ecae1a4
Tassany <tassany.onofre-de-oliveira@enac.fr>
2026-03-04 17:27:10 +0100

    Initial codes

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Generating a grid network
+netgenerate --grid --grid.number=5 \
+            --grid.length=200 \
+            --output-file=sumo/map.net.xml
+
+
---
+++ ns3/vanet_simulation.cc
@@ -0,0 +1,7 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/wave-module.h"
+
+
